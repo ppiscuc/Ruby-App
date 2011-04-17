7c2043c7169609730b3a58a96b0fc4170d86e01b
Paul Piscuc <paul.piscuc@gmail.com>
2011-04-17 22:35:38 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleman::Application.config.secret_token = '6d6ef309f601281ba047ca463d85e863a35d49fb306d11cfe7bfea74107fee34806bcc7122df53653c0dc308709a2a7946f8e39839d79b3ac9edec92f19903b9'

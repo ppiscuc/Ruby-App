6ee524d599a8afd5370d3bbd368e6ebe6e417e34
Paul Piscuc <cake.eater.mm@gmail.com>
2011-04-18 00:34:57 +0300

    added sphinx support

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -2,4 +2,11 @@ class Manual < ActiveRecord::Base
     attr_accessible :name, :value, :brief
     validates :name, :presence => true
     validates :value, :presence => true
+
+    define_index do
+        #fields
+        indexes value, :sortable => true
+        indexes brief, :sortable => true
+        #attributes
+    end
 end
